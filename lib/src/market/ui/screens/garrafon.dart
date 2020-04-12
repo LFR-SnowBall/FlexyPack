d1015c07d1800ddf57d49f9c6b7d73d6ed571d09
Luis Fernando Real Jaimes <luisitoreal01@gmail.com>
2020-04-12 05:31:52 -0500

    lista y diseno y llenado de tarjetas casi finalizado

--- lib/src/market/ui/screens/garrafon.dart
+++ lib/src/market/ui/screens/garrafon.dart
@@ -1,13 +1,14 @@
+import 'dart:convert';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flexypack/src/widgets/drawer.dart';
+import 'package:flexypack/src/market/model/listproduct.dart';
+import 'package:flexypack/src/widgets/listshop.dart';
 
 class GarrafonScreen extends StatefulWidget{
   @override
-  State<StatefulWidget> createState() {
+  State<StatefulWidget> createState() => _GarrafonScreenState();
     // TODO: implement createState
-    return _GarrafonScreenState();
-  }
-
 }
 
 //se implementa la interfaz empezando con un scaffold que es la inicialización de la misma continuando con los valores del widget
@@ -16,8 +17,11 @@ class GarrafonScreen extends StatefulWidget{
 
 class _GarrafonScreenState extends State<GarrafonScreen>{
   Drawere _drawere = new Drawere();
+  final ListWidgetsShop  _ListWidgetsShop = ListWidgetsShop();
+
   @override
   Widget build(BuildContext context) {
+    final _media = MediaQuery.of(context).size;
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.white,
@@ -30,30 +34,53 @@ class _GarrafonScreenState extends State<GarrafonScreen>{
             ),
         ],
         backgroundColor: Colors.green,
-        title: Image.asset('assets/images/logo.png',width: 200.0,height: 50.0,),
+        title: Text('Garrafon'),
         centerTitle: true,
       ),
-      body: GridView.count(
-        crossAxisCount: 4,
-        childAspectRatio: 1.0,
-        padding: const EdgeInsets.all(4.0),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
-        children: <Map<String,dynamic>>[
-          {
-            'icon':Icons.home,
-            'title':'dashboard'
-          },
-        ].map((Map<String,dynamic> item) {
+      body: GridView.builder(
+        itemCount: ListProducts().Garrafon.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          childAspectRatio: 0.70,
+          ), 
+        itemBuilder: (context,index){
           return Container(
-            child: Column(
-              children: <Widget>[
-                Icon(item['icon']),
-                Text(item['title']),
-              ],
-            ),
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(15.0),),
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                  children: <Widget>[
+                    Container(
+                      width: _media.width,
+                      height: _media.height*0.17,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(12))),
+                      child: _ListWidgetsShop.imageHeader(
+                        image: ListProducts().Garrafon[index]['Image'].toString(),
+                      ),
+                ),
+                ],
+                ),
+                Expanded(
+                  child: _ListWidgetsShop.productInfo(
+                    title: ListProducts().Garrafon[index]['Title'].toString(),
+                    description: ListProducts().Garrafon[index]['Description'].toString()
+                  ),
+                  ),
+                  ],
+                ),
+              ),
           );
-        }).toList())
+          
+        },
+        ),
     );
   }
 
